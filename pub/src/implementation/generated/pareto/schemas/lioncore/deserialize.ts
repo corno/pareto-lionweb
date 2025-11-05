42e00166d739bed7b91f1dff236f42430ef70faa
Corno Schraverus <corno@schraverus.com>
2025-11-05 21:09:34 +0100

    .

--- pub/src/implementation/generated/pareto/schemas/lioncore/deserialize.ts
+++ pub/src/implementation/generated/pareto/schemas/lioncore/deserialize.ts
@@ -1,13 +1,9 @@
 import * as _pa from 'exupery-core-alg'
 import * as _pd from 'exupery-core-dev'
 
-import * as _i_generic from "../../generic/unmarshall"
 import * as _i_out from "../../../../../interface/generated/pareto/schemas/lioncore/data_types/target"
-import * as _i_signatures from "../../../../../interface/generated/pareto/schemas/lioncore/unmarshall"
+import * as _i_signatures from "../../../../../interface/generated/pareto/schemas/lioncore/deserialize"
 
-
-import * as parse from "../../generic/parse/parse"
-import * as um from "./unmarshall"
 
 export const ID: _i_signatures._T_ID = ($, $p) => _pd.implement_me()
 export const M3: _i_signatures._T_M3 = ($, $p) => _pd.implement_me()
