e006c1008784e61777eddac08bf59affa26fa303
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/lioncore/unmarshall.ts
@@ -0,0 +1,7 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/lioncore/unmarshall"
+export const ID: t_signatures.ID = ($,) => _p.unreachable_code_path()
+export const Raw_Reference: t_signatures.Raw_Reference = ($,) => _p.unreachable_code_path()
+export const M3: t_signatures.M3 = ($,) => _p.unreachable_code_path()
