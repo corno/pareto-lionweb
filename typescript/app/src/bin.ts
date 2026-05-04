3ecbff5026b35151d4ecb6131136d57c2da4443c
Corno Schraverus <corno@schraverus.com>
2026-05-04 21:27:56 +0200

    pdt: ...

---
+++ typescript/app/src/bin.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S node --enable-source-maps
+
+import * as _pn from 'pareto-host-nodejs'
+import * as _pi from 'pareto-core/dist/interface'
+import * as _pq from 'pareto-core/dist/query'
+import * as _pc from 'pareto-core/dist/command'
+import __query from 'pareto-core/dist/__internals/async/query'
