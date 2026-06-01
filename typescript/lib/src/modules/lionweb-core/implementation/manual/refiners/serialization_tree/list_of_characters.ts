2359ce69fc97753334bab50c337ee7a15294b633
Corno Schraverus <corno@schraverus.com>
2026-06-01 22:03:48 +0200

    pdt: ...

--- typescript/lib/src/modules/lionweb-core/implementation/manual/refiners/serialization_tree/list_of_characters.ts
+++ typescript/lib/src/modules/lionweb-core/implementation/manual/refiners/serialization_tree/list_of_characters.ts
@@ -8,8 +8,7 @@ import * as d_in from "pareto-fountain-pen/dist/interface/generated/liana/schema
 
 //dependencies
 import * as r_serialization_tree_from_serialization_chunk from "./serialization_chunk"
-import * as r_serialization_chunk_from_list_of_characters from "../serialization_chunk/list_of_characters"
-import * as r_astn_parse_tree_from_list_of_characters from "astn-core/dist/implementation/manual/refiners/parse_tree/list_of_characters"
+import * as r_serialization_chunk_from_list_of_characters from "../../../to_be_generated/refiners/serialization_chunk/list_of_characters"
 
 export type Serialization_Tree = _pi.Refiner_With_Parameter<
     d_out.Serialization_Tree,
