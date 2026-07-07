7da973ed88b862bac50d43ab84a4433cc111f04a
Corno Schraverus <corno@schraverus.com>
2026-07-07 16:10:29 +0200

    pdt: ...

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -2,11 +2,12 @@ import * as p_i from 'pareto-core/interface/command'
 
 import * as resources_pareto from "pareto-resources/interface/resources"
 import * as resources_pareto_stream from "pareto-stream-api/interface/commands"
+import * as commands_pareto_application_api from "pareto-application-api/interface/resources"
 
 export namespace procedures {
 
     export type transform_lionweb_2024_1_language_to_astn = p_i.Command_Procedure<
-        resources_pareto.resources.commands.main,
+        commands_pareto_application_api.resources.commands.main,
         null,
         {
             'read file': resources_pareto.filesystem_unrestricted.queries.read_file
