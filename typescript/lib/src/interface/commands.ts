ee509a35edde6cc5933030ed2f8a8ce5d07b8599
Corno Schraverus <corno@schraverus.com>
2026-07-07 13:55:27 +0200

    pdt: ...

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,7 +1,7 @@
 import * as p_i from 'pareto-core/interface/command'
 
 import * as resources_pareto from "pareto-resources/interface/resources"
-import * as resources_pareto_stream from "pareto-stream/interface/commands"
+import * as resources_pareto_stream from "pareto-stream-api/interface/commands"
 
 export namespace procedures {
 
