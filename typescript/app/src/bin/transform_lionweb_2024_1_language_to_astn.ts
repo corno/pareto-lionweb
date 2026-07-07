1c135a8713937cd7cd5b040fdfaf6109347716a6
Corno Schraverus <corno@schraverus.com>
2026-07-07 15:29:01 +0200

    pdt: ...

--- typescript/app/src/bin/transform_lionweb_2024_1_language_to_astn.ts
+++ typescript/app/src/bin/transform_lionweb_2024_1_language_to_astn.ts
@@ -2,7 +2,7 @@
 
 import * as p_h from 'pareto-host-nodejs/index'
 
-import * as rs_filesystem_unrestricted from "pareto-host-nodejs/file_system_unrestricted/index"
+import * as rs_filesystem_unrestricted from "pareto-resource-filesystem-unrestricted/index"
 import * as rs_stream from "pareto-resource-stream/index"
 
 import { $$ as procedure } from "lib/implementation/manual/commands/transform_lionweb_2024_1_language_to_astn"
